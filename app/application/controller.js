9fac7a298cf68fb12dc246948e0a79c155d6b1e0
narlt <nicholas.arlt@evhc.net>
2017-04-12 13:26:11 -0700

    Continued with Ember School course, basic CRUDL app at this point

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,15 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  title: 'chef Tracker',
+  title: 'Chef Tracker',
+  newChef: null,
+  menuLength: Ember.computed.alias('model.length'),
+  availableChefs: Ember.computed.filterBy('model', 'isAvailable', true),
 
   actions: {
       unAvailable(chef){
         Ember.set(chef, 'isAvailable', false);
+        chef.save();
       },
       isAvailable(chef){
         Ember.set(chef, 'isAvailable', true);
+        chef.save();
+      },
+      saveNewChef(newChef){
+        this.store.createRecord('chef', {
+          isAvailable: false,
+          name: this.get('newChef')
+        }).save();
+        this.set('newChef', '');
+      },
+      deleteChef(chef){
+        chef.destroyRecord();
       }
+
   }
 
 
--- app/models/chef.js
+++ app/models/chef.js
@@ -4,3 +4,4 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   isAvailable: DS.attr('boolean')
 });
+
