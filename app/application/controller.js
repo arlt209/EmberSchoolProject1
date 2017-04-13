ce85a5f6bca0e3a5c636b1dc717e533a69692443
narlt <nicholas.arlt@evhc.net>
2017-04-13 13:15:10 -0700

    Completed the first and second challenges

--- app/application/controller.js
+++ app/application/controller.js
@@ -2,9 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   title: 'Chef Tracker',
-  newChef: null,
-  menuLength: Ember.computed.alias('model.length'),
+  totalChefs: Ember.computed.alias('model.length'),
   availableChefs: Ember.computed.filterBy('model', 'isAvailable', true),
+  getStudents: Ember.computed.mapBy('model', 'students'),
+  totalStudents: Ember.computed.sum('getStudents'),
+
+
 
   actions: {
       unAvailable(chef){
@@ -15,7 +18,7 @@ export default Ember.Controller.extend({
         Ember.set(chef, 'isAvailable', true);
         chef.save();
       },
-      saveNewChef(newChef){
+      saveNewChef(){
         this.store.createRecord('chef', {
           isAvailable: false,
           name: this.get('newChef')
@@ -25,10 +28,15 @@ export default Ember.Controller.extend({
       deleteChef(chef){
         chef.destroyRecord();
       },
-      removeStudent(chef.students){
-        chef.students.destroyRecord();
+      addStudent(chef){
+        chef.set('students', chef.get('students') + 1);
+        chef.save();
+      },
+      removeStudent(chef){
+        if (chef.get('students') > 0) {
+          chef.set('students', chef.get('students') - 1);
+        }
+        chef.save();
       }
   }
-
-
 });
